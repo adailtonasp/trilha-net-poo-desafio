2d23f1e8ec5c5c4c45fe13d035b1bc11a8398b9d
adailtonasp <adailton-hag@hotmail.com>
2022-12-21 20:00:33 -0300

    modificações pendentes concluida

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("40028922", "model1", "98959856", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("appLoja2");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone");
+Smartphone iphone = new Iphone("32281516", "gs", "854644", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("appLoja1");
